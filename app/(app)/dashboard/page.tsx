c8bf414d9269038cd75f1b184d4755787006eee1
Rishabh Nema <182422143+rishabhnema21@users.noreply.github.com>
2026-01-06 18:56:09 +0530

    sign in page frontend integrate and ui

---
+++ app/(app)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Hey! Welcome to Unsaid</div>
+  )
+}
+
+export default page
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,131 @@
-import React from 'react'
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
+// import {
+//   InputGroup,
+//   InputGroupAddon,
+//   InputGroupText,
+//   InputGroupTextarea,
+// } from "@/components/ui/input-group"
 
 const page = () => {
-  return (
-    <div>Sign In Page</div>
-  )
-}
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
 
-export default page
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const response = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+    if (response?.error) {
+      toast.error("Sign In Failed")
+    } 
+
+    if (response?.url) {
+      router.replace("/dashboard");
+    }
+  };
+
+  return (
+    <div className="flex justify-evenly items-center min-h-screen bg-[#f8f9fa]">
+
+      <div className="w-1/2 flex justify-center items-center">
+        <div className="h-[40vh]">
+          <img className="h-full w-full" src="/unsaidasset01.png" alt="random" />
+        </div>
+        <div className="w-1/2">
+          <h2 className="text-5xl font-bold">Let people tell you what they really think.</h2>
+          <h4 className="mt-3 text-2xl text-zinc-600 font-semibold italic">Anonymous messages. Real thoughts.</h4>
+        </div>
+      </div>
+
+      <div className="w-1/2 theme-font max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Back to Unsaid.
+          </h1>
+          <p className="mb-4">Sign In to continue the Mystery</p>
+        </div>
+
+        <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+
+            <Controller
+              name="identifier"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="email/username">Email/Username</FieldLabel>
+                  <Input {...field} placeholder="Enter here." />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    placeholder="between you & Unsaid"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+          <Button type="submit">
+            Sign In
+          </Button>
+        </form>
+
+        <div className="text-center mt-4">
+          <p>
+            New to the silence? → {" "}
+            <Link href="/sign-up" className="text-zinc-400 hover:text-zinc-800">
+              Join Unsaid
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
